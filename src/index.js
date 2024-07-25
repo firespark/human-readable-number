2757d1ea67e86f4ba297d93244d5217a2f9c1bcf
Elena Markova <fireleit@gmail.com>
2024-07-25 15:27:00 +0400

    init: add algorithm for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
+    // 0, 10 - 19 - exceptions array
+    // 1-9 - units array
+    // 20, 30 .. 90 - tens array
+
+    // Check number. If in exceptions array, return its value
+
+    // Else check number length:
+
+    // If 1, return units[number - 1]
+
+    // If 2, return tens[number[0] - 1] + units[number[1] - 1]
+
+    // If 3, returns units[number[0] - 1] + 'hundred' + tens[number[0] - 1] + units[number[1] - 1]
+
+    console.log('111');
+
+
   
 }
